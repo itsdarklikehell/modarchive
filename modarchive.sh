bf9a90fee951245403941b19b60a1093c528c952
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-05-24 06:09:45 +0200

    modified:   modarchive.sh

--- modarchive.sh
+++ modarchive.sh
@@ -47,6 +47,10 @@ Modarchive Jukebox can be used with one of the following options:
            audacious  This is an X11 player. Uses modplug tu decode files
            opencp     This is Open Cubic Player, a console/x11 classic player. 
                       It's really buggy but, who cares?
+		   sunvox	  SunVox is a small, fast and powerful modular synthesizer with pattern-based sequencer (tracker). 
+		   			  It is a tool for those people who like to compose music wherever they are, whenever they wish. On any device.
+					  On any system. And it's free for most of the systems, except the Android and iOS versions which are like 9 dollars or so (I think).
+
          
    -s <section> Play from selected section: Can be one of this 
           featured    These modules have been nominated by the crew for either 
