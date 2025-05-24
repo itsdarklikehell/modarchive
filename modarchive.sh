95bfd3d7c148e165e9060dacad7470e92d321a03
Bauke Molenaar <bauke.molenaar@gmail.com>
2025-05-25 01:11:39 +0200

    modified:   .gitignore 	modified:   modarchive.sh

--- modarchive.sh
+++ modarchive.sh
@@ -40,266 +40,278 @@ fi
 # --- Whiptail Menu Logic ---
 
 # Main Menu
-MAIN_CHOICE=$(whiptail --title "Modarchive Jukebox" --menu "Choose an option:" $LINES $COLUMNS $((LINES - 8)) \
-	"section" "Play from a specific section" \
-	"artist" "Search by artist" \
-	"module" "Search by module title/filename" \
-	"random" "Play a random module" \
-	"settings" "Configure player, tracks, shuffle" \
-	"exit" "Exit the jukebox" 3>&1 1>&2 2>&3)
-
-exitstatus=$?
-if [ $exitstatus != 0 ]; then
-	echo "User cancelled."
-	exit 1
-fi
-
-case $MAIN_CHOICE in
-section)
-	SECTION_CHOICE=$(whiptail --title "Select Section" --menu "Choose a section:" $LINES $COLUMNS $((LINES - 8)) \
-		"featured" "Featured modules" \
-		"favourites" "Favourite modules" \
-		"downloads" "Top downloaded modules" \
-		"topscore" "Top scored modules" \
-		"newadd" "New additions" \
-		"newratings" "Recent rated modules" 3>&1 1>&2 2>&3)
+while true; do
+	MAIN_CHOICE=$(
+		whiptail --title "Modarchive Jukebox" --menu "Choose an option:" $LINES $COLUMNS $((LINES - 8)) \
+			"random" "Play a random module" \
+			"section" "Play from a specific section" \
+			"artist" "Search by artist" \
+			"module" "Search by module title/filename" \
+			"settings" "Configure player, tracks, shuffle" \
+			"exit" "Exit the jukebox" 3>&1 1>&2 2>&3
+	)
 
 	exitstatus=$?
 	if [ $exitstatus != 0 ]; then
-		echo "User cancelled section selection."
-		exit 1
+		# User cancelled. Go back to main menu
+		continue
 	fi
 
-	case $SECTION_CHOICE in
-	featured)
-		MODURL="http://modarchive.org/index.php?request=view_chart&query=featured"
-		MODLIST="list.featured"
-		;;
-	favourites)
-		MODURL="http://modarchive.org/index.php?request=view_top_favourites"
-		MODLIST="list.favourites"
-		;;
-	downloads)
-		MODURL="http://modarchive.org/index.php?request=view_chart&query=tophits"
-		MODLIST="list.downloads"
-		;;
-	topscore)
-		MODURL="http://modarchive.org/index.php?request=view_chart&query=topscore"
-		MODLIST="list.topscore"
-		;;
-	newadd)
-		MODURL="http://modarchive.org/index.php?request=view_actions_uploads"
-		MODLIST="list.newadd"
-		PAGES='0' # New additions page structure is different
-		;;
-	newratings)
-		MODURL="http://modarchive.org/index.php?request=view_actions_ratings"
-		MODLIST="list.newratings"
-		PAGES='0' # New ratings page structure is different
-		;;
-	esac
-	;;
-artist)
-	ARTIST_QUERY=$(whiptail --title "Search Artist" --inputbox "Enter artist name:" $LINES $COLUMNS "" 3>&1 1>&2 2>&3)
-	exitstatus=$?
-	if [ $exitstatus != 0 ]; then
-		echo "User cancelled artist search."
-		exit 1
-	fi
-	if [ -z "$ARTIST_QUERY" ]; then
-		whiptail --msgbox "Artist name cannot be empty." $LINES $COLUMNS
-		exit 1
-	fi
-	QUERY=$(echo "${ARTIST_QUERY}" | sed 's/ /+/g')
-	QUERYURL="http://modarchive.org/index.php?query=$QUERY&submit=Find&request=search&search_type=search_artist"
-	ARTISTNO=$(curl -s "$QUERYURL" | grep -A 10 "Search Results" | grep member.php | sed 's/>/>\n/g' | head -1 | cut -d "?" -f 2 | cut -d "\"" -f 1)
-	if [ -z "$ARTISTNO" ]; then
-		whiptail --msgbox "The artist search returned no results." $LINES $COLUMNS
-		exit 1
-	fi
-	MODURL="http://modarchive.org/index.php?request=view_artist_modules&query=${ARTISTNO}"
-	MODLIST="artist.${ARTIST_QUERY}"
-	;;
-module)
-	MODULE_QUERY=$(whiptail --title "Search Module" --inputbox "Enter module title or filename:" $LINES $COLUMNS "" 3>&1 1>&2 2>&3)
-	exitstatus=$?
-	if [ $exitstatus != 0 ]; then
-		echo "User cancelled module search."
-		exit 1
-	fi
-	if [ -z "$MODULE_QUERY" ]; then
-		whiptail --msgbox "Module title/filename cannot be empty." $LINES $COLUMNS
-		exit 1
-	fi
-	MODURL="http://modarchive.org/index.php?request=search&query=${MODULE_QUERY}&submit=Find&search_type=filename_or_songtitle"
-	MODLIST="search.${MODULE_QUERY}"
-	;;
-random)
-	RANDOMSONG="true"
-	MODURL="http://modarchive.org/index.php?request=view_random"
-	PAGES='0' # Random doesn't use pagination like lists
-	;;
-settings)
-	SETTINGS_CHOICE=$(whiptail --title "Settings" --menu "Configure options:" $LINES $COLUMNS $((LINES - 8)) \
-		"player" "Select player profile" \
-		"tracks" "Set number of tracks to play" \
-		"shuffle" "Toggle shuffle mode" \
-		"back" "Back to main menu" 3>&1 1>&2 2>&3)
+	case $MAIN_CHOICE in
+	section)
+		SECTION_CHOICE=$(whiptail --title "Select Section" --menu "Choose a section:" $LINES $COLUMNS $((LINES - 8)) \
+			"featured" "Featured modules" \
+			"favourites" "Favourite modules" \
+			"downloads" "Top downloaded modules" \
+			"topscore" "Top scored modules" \
+			"newadd" "New additions" \
+			"newratings" "Recent rated modules" 3>&1 1>&2 2>&3)
 
-	exitstatus=$?
-	if [ $exitstatus != 0 ]; then
-		# User cancelled settings menu, go back to main menu
-		# Re-run the script or loop back if implemented
-		echo "User cancelled settings, exiting for now. Re-run to choose again."
-		exit 1 # Simple exit for now, a loop would be better
-	fi
+		exitstatus=$?
+		if [ $exitstatus != 0 ]; then
+			# User cancelled section selection. Go back to main menu
+			continue
+		fi
 
-	case $SETTINGS_CHOICE in
-	player)
-		PLAYER_CHOICE=$(whiptail --title "Select Player" --menu "Choose a player:" $LINES $COLUMNS $((LINES - 8)) \
-			"mikmod" "Console player (default)" \
-			"audacious" "X11 player" \
-			"opencp" "Open Cubic Player" \
-			"sunvox" "SunVox" \
-			"openmpt123" "OpenMPT123" \
-			"openmpt" "OpenMPT" \
-			"vlc" "vlc player" \
-			"cvlc" "console vlc player" 3>&1 1>&2 2>&3)
+		case $SECTION_CHOICE in
+		featured)
+			MODURL="http://modarchive.org/index.php?request=view_chart&query=featured"
+			MODLIST="list.featured"
+			;;
+		favourites)
+			MODURL="http://modarchive.org/index.php?request=view_top_favourites"
+			MODLIST="list.favourites"
+			;;
+		downloads)
+			MODURL="http://modarchive.org/index.php?request=view_chart&query=tophits"
+			MODLIST="list.downloads"
+			;;
+		topscore)
+			MODURL="http://modarchive.org/index.php?request=view_chart&query=topscore"
+			MODLIST="list.topscore"
+			;;
+		newadd)
+			MODURL="http://modarchive.org/index.php?request=view_actions_uploads"
+			MODLIST="list.newadd"
+			PAGES='0' # New additions page structure is different
+			;;
+		newratings)
+			MODURL="http://modarchive.org/index.php?request=view_actions_ratings"
+			MODLIST="list.newratings"
+			PAGES='0' # New ratings page structure is different
+			;;
+		esac
+		;;
+	artist)
+		ARTIST_QUERY=$(whiptail --title "Search Artist" --inputbox "Enter artist name:" $LINES $COLUMNS "" 3>&1 1>&2 2>&3)
+		exitstatus=$?
+		if [ $exitstatus != 0 ]; then
+			echo "User cancelled artist search."
+			exit 1
+		fi
+		if [ -z "$ARTIST_QUERY" ]; then
+			whiptail --msgbox "Artist name cannot be empty." $LINES $COLUMNS
+			exit 1
+		fi
+		QUERY=$(echo "${ARTIST_QUERY}" | sed 's/ /+/g')
+		QUERYURL="http://modarchive.org/index.php?query=$QUERY&submit=Find&request=search&search_type=search_artist"
+		ARTISTNO=$(curl -s "$QUERYURL" | grep -A 10 "Search Results" | grep member.php | sed 's/>/>\n/g' | head -1 | cut -d "?" -f 2 | cut -d "\"" -f 1)
+		if [ -z "$ARTISTNO" ]; then
+			whiptail --msgbox "The artist search returned no results." $LINES $COLUMNS
+			exit 1
+		fi
+		MODURL="http://modarchive.org/index.php?request=view_artist_modules&query=${ARTISTNO}"
+		MODLIST="artist.${ARTIST_QUERY}"
+		;;
+	module)
+		MODULE_QUERY=$(whiptail --title "Search Module" --inputbox "Enter module title or filename:" $LINES $COLUMNS "" 3>&1 1>&2 2>&3)
+		exitstatus=$?
+		if [ $exitstatus != 0 ]; then
+			echo "User cancelled module search."
+			exit 1
+		fi
+		if [ -z "$MODULE_QUERY" ]; then
+			whiptail --msgbox "Module title/filename cannot be empty." $LINES $COLUMNS
+			exit 1
+		fi
+		MODURL="http://modarchive.org/index.php?request=search&query=${MODULE_QUERY}&submit=Find&search_type=filename_or_songtitle"
+		MODLIST="search.${MODULE_QUERY}"
+		;;
+	random)
+		RANDOMSONG="true"
+		MODURL="http://modarchive.org/index.php?request=view_random"
+		PAGES='0' # Random doesn't use pagination like lists
+		;;
+	settings)
+		SETTINGS_CHOICE=$(whiptail --title "Settings" --menu "Configure options:" $LINES $COLUMNS $((LINES - 8)) \
+			"player" "Select player profile" \
+			"tracks" "Set number of tracks to play" \
+			"shuffle" "Toggle shuffle mode" \
+			"back" "Back to main menu" 3>&1 1>&2 2>&3)
 
 		exitstatus=$?
 		if [ $exitstatus != 0 ]; then
 			echo "User cancelled player selection."
-			# Decide whether to exit or go back to settings menu
-			exit 1 # Simple exit for now
+			continue # Go back to main menu
 		fi
 
-		case $PLAYER_CHOICE in
-		audacious)
-			cat <<EOF >>~/.modarchiverc
+		case $SETTINGS_CHOICE in
+		player)
+			PLAYER_CHOICE=$(whiptail --title "Select Player" --menu "Choose a player:" $LINES $COLUMNS $((LINES - 8)) \
+				"mikmod" "Console player (default)" \
+				"audacious" "X11 player" \
+				"opencp" "Open Cubic Player" \
+				"sunvox" "SunVox" \
+				"openmpt123" "OpenMPT123" \
+				"openmpt" "OpenMPT" \
+				"vlc" "vlc player" \
+				"cvlc" "console vlc player" 3>&1 1>&2 2>&3)
+
+			exitstatus=$?
+			if [ $exitstatus != 0 ]; then
+				echo "User cancelled player selection."
+				continue # Go back to main menu
+			fi
+
+			case $PLAYER_CHOICE in
+			audacious)
+				cat <<EOF >~/.modarchiverc
+#!/bin/bash
 MODPATH='$HOME/sunvox/examples/modarchive'
 
 PLAYER='$(which audacious)'
 PLAYEROPTS='-e'
 PLAYERBG='true'
 EOF
-			whiptail --msgbox "audacious selected. Ensure 'audacious' command is in your PATH and can play modules directly." $LINES $COLUMNS
-			;;
-		mikmod)
-			cat <<EOF >>~/.modarchiverc
+				whiptail --msgbox "audacious selected. Ensure 'audacious' command is in your PATH and can play modules directly." $LINES $COLUMNS
+				;;
+			mikmod)
+				cat <<EOF >~/.modarchiverc
+#!/bin/bash
 MODPATH='$HOME/sunvox/examples/modarchive'
 
 PLAYER='$(which mikmod)'
 PLAYEROPTS='-i -X --surround --hqmixer -f 48000 -X'
 PLAYERBG='false'
 EOF
-			whiptail --msgbox "mikmod selected. Ensure 'mikmod' command is in your PATH and can play modules directly." $LINES $COLUMNS
-			;;
-		opencp)
-			cat <<EOF >>~/.modarchiverc
+				whiptail --msgbox "mikmod selected. Ensure 'mikmod' command is in your PATH and can play modules directly." $LINES $COLUMNS
+				;;
+			opencp)
+				cat <<EOF >~/.modarchiverc
+#!/bin/bash
 MODPATH='$HOME/sunvox/examples/modarchive'
 
 PLAYER='$(which ocp)'
 PLAYEROPTS='-p'
 PLAYERBG='false'
 EOF
-			whiptail --msgbox "Open Cubic Player selected. Ensure 'ocp' command is in your PATH and can play modules directly." $LINES $COLUMNS
-			;;
-		sunvox)
-			cat <<EOF >>~/.modarchiverc
+				whiptail --msgbox "Open Cubic Player selected. Ensure 'ocp' command is in your PATH and can play modules directly." $LINES $COLUMNS
+				;;
+			sunvox)
+				cat <<EOF >~/.modarchiverc
+#!/bin/bash
 MODPATH='$HOME/sunvox/examples/modarchive'
 
 PLAYER='$(which sunvox)'
 PLAYEROPTS='-p'
 PLAYERBG='false'
 EOF
-			whiptail --msgbox "SunVox selected. Ensure 'sunvox' command is in your PATH and can play modules directly." $LINES $COLUMNS
-			;;
-		openmpt123)
-			cat <<EOF >>~/.modarchiverc
+				whiptail --msgbox "SunVox selected. Ensure 'sunvox' command is in your PATH and can play modules directly." $LINES $COLUMNS
+				;;
+			openmpt123)
+				cat <<EOF >~/.modarchiverc
+#!/bin/bash
 MODPATH='$HOME/sunvox/examples/modarchive'
 PLAYER='$(which openmpt123)'
 PLAYEROPTS=''
 PLAYERBG='false'
 EOF
-			whiptail --msgbox "OpenMPT123 selected. Ensure 'openmpt123' command is in your PATH and can play modules directly." $LINES $COLUMNS
-			;;
-		openmpt)
-			cat <<EOF >>~/.modarchiverc
+				whiptail --msgbox "OpenMPT123 selected. Ensure 'openmpt123' command is in your PATH and can play modules directly." $LINES $COLUMNS
+				;;
+			openmpt)
+				cat <<EOF >~/.modarchiverc
+#!/bin/bash
 MODPATH='$HOME/sunvox/examples/modarchive'
 PLAYER='$(which openmpt)'
 PLAYEROPTS='--play'
 PLAYERBG='false'
 EOF
-			whiptail --msgbox "OpenMPT selected. Ensure 'openmpt' command is in your PATH and can play modules directly." $LINES $COLUMNS
-			;;
-		vlc)
-			cat <<EOF >>~/.modarchiverc
+				whiptail --msgbox "OpenMPT selected. Ensure 'openmpt' command is in your PATH and can play modules directly." $LINES $COLUMNS
+				;;
+			vlc)
+				cat <<EOF >~/.modarchiverc
+#!/bin/bash
 MODPATH='$HOME/sunvox/examples/modarchive'
 PLAYER='$(which vlc)'
 PLAYEROPTS='--play-and-exit'
 PLAYERBG='false'
 EOF
-			whiptail --msgbox "VLC selected. Ensure 'vlc' command is in your PATH and can play modules directly." $LINES $COLUMNS
-			;;
-		cvlc)
-			cat <<EOF >>~/.modarchiverc
+				whiptail --msgbox "VLC selected. Ensure 'vlc' command is in your PATH and can play modules directly." $LINES $COLUMNS
+				;;
+			cvlc)
+				cat <<EOF >~/.modarchiverc
+#!/bin/bash
 MODPATH='$HOME/sunvox/examples/modarchive'
 PLAYER='$(which cvlc)'
 PLAYEROPTS='--play-and-exit'
 PLAYERBG='false'
 EOF
-			whiptail --msgbox "VLC selected. Ensure 'vlc' command is in your PATH and can play modules directly." $LINES $COLUMNS
+				whiptail --msgbox "VLC selected. Ensure 'vlc' command is in your PATH and can play modules directly." $LINES $COLUMNS
+				;;
+			*)
+				whiptail --msgbox "ERROR: ${PLAYER_CHOICE} player is not supported." $LINES $COLUMNS
+				exit 1
+				;;
+			esac
+			# After setting player, ideally go back to settings menu or main menu
+			# For simplicity now, we'll let it proceed, but a loop would be better
 			;;
-		*)
-			whiptail --msgbox "ERROR: ${PLAYER_CHOICE} player is not supported." $LINES $COLUMNS
-			exit 1
+		tracks)
+			TRACKS_INPUT=$(whiptail --title "Number of Tracks" --inputbox "Enter number of tracks to play (0 for all):" 8 78 "0" 3>&1 1>&2 2>&3)
+			exitstatus=$?
+			if [ $exitstatus != 0 ]; then
+				echo "User cancelled track number input."
+				exit 1 # Simple exit for now
+			fi
+			if [[ "$TRACKS_INPUT" =~ ^[0-9]+$ ]]; then
+				TRACKSNUM=${TRACKS_INPUT}
+			else
+				whiptail --msgbox "Invalid input. Please enter a number." $LINES $COLUMNS
+				exit 1 # Exit on invalid input
+			fi
+			# After setting tracks, ideally go back to settings menu or main menu
+			# For simplicity now, we'll let it proceed
+			;;
+		shuffle)
+			if (whiptail --title "Shuffle" --yesno "Enable shuffle mode?" $LINES $COLUMNS); then
+				SHUFFLE="true"
+				whiptail --msgbox "Shuffle enabled." $LINES $COLUMNS
+			else
+				SHUFFLE=""
+				whiptail --msgbox "Shuffle disabled." $LINES $COLUMNS
+			fi
+			# After setting shuffle, ideally go back to settings menu or main menu
+			# For simplicity now, we'll let it proceed
+			;;
+		back)
+			# This case would ideally loop back to the main menu
+			echo "Going back to main menu (requires script loop)."
+			exit 1 # Simple exit for now, needs loop
 			;;
 		esac
-		# After setting player, ideally go back to settings menu or main menu
-		# For simplicity now, we'll let it proceed, but a loop would be better
+		# After settings, ideally loop back to main menu or proceed if main choice was already made
+		# For simplicity now, we'll let it proceed if MODURL is set
 		;;
-	tracks)
-		TRACKS_INPUT=$(whiptail --title "Number of Tracks" --inputbox "Enter number of tracks to play (0 for all):" 8 78 "0" 3>&1 1>&2 2>&3)
-		exitstatus=$?
-		if [ $exitstatus != 0 ]; then
-			echo "User cancelled track number input."
-			exit 1 # Simple exit for now
-		fi
-		if [[ "$TRACKS_INPUT" =~ ^[0-9]+$ ]]; then
-			TRACKSNUM=${TRACKS_INPUT}
-		else
-			whiptail --msgbox "Invalid input. Please enter a number." $LINES $COLUMNS
-			exit 1 # Exit on invalid input
-		fi
-		# After setting tracks, ideally go back to settings menu or main menu
-		# For simplicity now, we'll let it proceed
-		;;
-	shuffle)
-		if (whiptail --title "Shuffle" --yesno "Enable shuffle mode?" $LINES $COLUMNS); then
-			SHUFFLE="true"
-			whiptail --msgbox "Shuffle enabled." $LINES $COLUMNS
-		else
-			SHUFFLE=""
-			whiptail --msgbox "Shuffle disabled." $LINES $COLUMNS
-		fi
-		# After setting shuffle, ideally go back to settings menu or main menu
-		# For simplicity now, we'll let it proceed
-		;;
-	back)
-		# This case would ideally loop back to the main menu
-		echo "Going back to main menu (requires script loop)."
-		exit 1 # Simple exit for now, needs loop
+	exit)
+		echo "Exiting Modarchive Jukebox."
+		exit 0
 		;;
 	esac
-	# After settings, ideally loop back to main menu or proceed if main choice was already made
-	# For simplicity now, we'll let it proceed if MODURL is set
-	;;
-exit)
-	echo "Exiting Modarchive Jukebox."
-	exit 0
-	;;
-esac
-
+	# After the switch, ideally loop back to the main menu or proceed if MODURL is set
+	if [ -z "$MODURL" ]; then
+		continue
+	fi
+done
 # --- End of Whiptail Menu Logic ---
 
 # Check if a source was selected (MODURL should be set unless user exited or cancelled early)
